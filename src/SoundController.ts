742333ba24df31a9808e09cf6964d5f8dc74648e
Birdman1104 <arsenmazmanyan11@gmail.com>
2024-12-24 01:37:30 +0400

    Sounds.

--- src/SoundController.ts
+++ src/SoundController.ts
@@ -1,9 +1,11 @@
 import { lego } from '@armathai/lego';
 import { Howl } from 'howler';
-import { MainGameEvents } from './events/MainEvents';
+import { BoardEvents, MainGameEvents, UIEvents } from './events/MainEvents';
 import { SoundModelEvents } from './events/ModelEvents';
 import { SoundState } from './models/SoundModel';
 import { CLICK_SOUND } from './sounds/click';
+import { ITEM_SOUND } from './sounds/itemPut';
+import { swoosh } from './sounds/swoosh';
 
 class SoundControl {
     private sounds: { [key: string]: Howl };
@@ -14,15 +16,31 @@ class SoundControl {
 
         lego.event
             .on(MainGameEvents.MuteUpdate, this.focusChange, this)
-            .on(SoundModelEvents.StateUpdate, this.onSoundStateUpdate, this);
+            .on(SoundModelEvents.StateUpdate, this.onSoundStateUpdate, this)
+            .on(BoardEvents.ZoneClicked, this.playClick, this)
+            .on(BoardEvents.LockClick, this.playClick, this)
+            .on(BoardEvents.OkClick, this.playOk, this)
+            .on(BoardEvents.NoClick, this.playClick, this)
+            .on(UIEvents.CardClick, this.playClick, this)
+            .on(UIEvents.CarouselUpdate, this.playSwoosh, this);
     }
 
     public loadSounds(): void {
         this.sounds.click = new Howl({ src: CLICK_SOUND });
+        this.sounds.item = new Howl({ src: ITEM_SOUND });
+        this.sounds.swoosh = new Howl({ src: swoosh });
     }
 
     private playClick(): void {
         this.sounds.click.play();
+    }
+
+    private playOk(): void {
+        this.sounds.item.play();
+    }
+
+    private playSwoosh(): void {
+        this.sounds.swoosh.play();
     }
 
     private focusChange(outOfFocus: boolean): void {
